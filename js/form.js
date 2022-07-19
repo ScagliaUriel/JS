6ccf0f6f222e3e253292e7fc41e2d1e3849017c7
Nicetastic <urielscagliarcc@gmail.com>
2022-07-19 20:41:30 -0300

    form

--- js/form.js
+++ js/form.js
@@ -1,71 +1,106 @@
-const nombre = document.getElementById("myname");
-const apellidos = document.getElementById("surname");
-const correo = document.getElementById("email");
-const celular = document.getElementById("mobile");
-const contrasenia = document.getElementById("password");
-const contrasenia2 = document.getElementById("repeatPassword");
-const terminosYcondiciones = document.getElementById("termsAndConditions");
-const form = document.getElementById("form");
-const listInputs = document.querySelectorAll(".form-input");
+const formulario = document.getElementById('formulario');
+const inputs = document.querySelectorAll('#formulario input');
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  let condicion = validacionForm();
-  if (condicion) {
-    enviarFormulario();
-  }
+const expresiones = {
+	usuario: /^[a-zA-Z0-9\_\-]{4,16}$/, // Letras, numeros, guion y guion_bajo
+	nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
+	password: /^.{4,12}$/, // 4 a 12 digitos.
+	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+	telefono: /^\d{7,14}$/ // 7 a 14 numeros.
+}
+
+const campos = {
+	usuario: false,
+	nombre: false,
+	password: false,
+	correo: false,
+	telefono: false
+}
+
+const validarFormulario = (e) => {
+	switch (e.target.name) {
+		case "usuario":
+			validarCampo(expresiones.usuario, e.target, 'usuario');
+		break;
+		case "nombre":
+			validarCampo(expresiones.nombre, e.target, 'nombre');
+		break;
+		case "password":
+			validarCampo(expresiones.password, e.target, 'password');
+			validarPassword2();
+		break;
+		case "password2":
+			validarPassword2();
+		break;
+		case "correo":
+			validarCampo(expresiones.correo, e.target, 'correo');
+		break;
+		case "telefono":
+			validarCampo(expresiones.telefono, e.target, 'telefono');
+		break;
+	}
+}
+
+const validarCampo = (expresion, input, campo) => {
+	if(expresion.test(input.value)){
+		document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-incorrecto');
+		document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-correcto');
+		document.querySelector(`#grupo__${campo} i`).classList.add('fa-check-circle');
+		document.querySelector(`#grupo__${campo} i`).classList.remove('fa-times-circle');
+		document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.remove('formulario__input-error-activo');
+		campos[campo] = true;
+	} else {
+		document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-incorrecto');
+		document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-correcto');
+		document.querySelector(`#grupo__${campo} i`).classList.add('fa-times-circle');
+		document.querySelector(`#grupo__${campo} i`).classList.remove('fa-check-circle');
+		document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.add('formulario__input-error-activo');
+		campos[campo] = false;
+	}
+}
+
+const validarPassword2 = () => {
+	const inputPassword1 = document.getElementById('password');
+	const inputPassword2 = document.getElementById('password2');
+
+	if(inputPassword1.value !== inputPassword2.value){
+		document.getElementById(`grupo__password2`).classList.add('formulario__grupo-incorrecto');
+		document.getElementById(`grupo__password2`).classList.remove('formulario__grupo-correcto');
+		document.querySelector(`#grupo__password2 i`).classList.add('fa-times-circle');
+		document.querySelector(`#grupo__password2 i`).classList.remove('fa-check-circle');
+		document.querySelector(`#grupo__password2 .formulario__input-error`).classList.add('formulario__input-error-activo');
+		campos['password'] = false;
+	} else {
+		document.getElementById(`grupo__password2`).classList.remove('formulario__grupo-incorrecto');
+		document.getElementById(`grupo__password2`).classList.add('formulario__grupo-correcto');
+		document.querySelector(`#grupo__password2 i`).classList.remove('fa-times-circle');
+		document.querySelector(`#grupo__password2 i`).classList.add('fa-check-circle');
+		document.querySelector(`#grupo__password2 .formulario__input-error`).classList.remove('formulario__input-error-activo');
+		campos['password'] = true;
+	}
+}
+
+inputs.forEach((input) => {
+	input.addEventListener('keyup', validarFormulario);
+	input.addEventListener('blur', validarFormulario);
 });
 
-function validacionForm() {
-  form.lastElementChild.innerHTML = "";
-  let condicion = true;
-  listInputs.forEach((element) => {
-    element.lastElementChild.innerHTML = "";
-  });
+formulario.addEventListener('submit', (e) => {
+	e.preventDefault();
 
-  if (nombre.value.length < 1 || nombre.value.trim() == "") {
-    mostrarMensajeError("myname", "Nombre no valido*");
-    condicion = false;
-  }
-  if (apellidos.value.length < 1 || apellidos.value.trim() == "") {
-    mostrarMensajeError("surname", "Apellido no valido");
-    condicion = false;
-  }
-  if (correo.value.length < 1 || correo.value.trim() == "") {
-    mostrarMensajeError("email", "Correo no valido*");
-    condicion = false;
-  }
-  if (
-    celular.value.length != 9 ||
-    celular.value.trim() == "" ||
-    isNaN(celular.value)
-  ) {
-    mostrarMensajeError("mobile", "Celular no valido*");
-    condicion = false;
-  }
-  if (contrasenia.value.length < 1 || contrasenia.value.trim() == "") {
-    mostrarMensajeError("password", "Contraseña no valido*");
-    condicion = false;
-  }
-  if (contrasenia2.value != contrasenia.value) {
-    mostrarMensajeError("repeatPassword", "Contraseña error*");
-    condicion = false;
-  }
-  if (!terminosYcondiciones.checked) {
-    mostrarMensajeError("termsAndConditions", "Acepte*");
-    condicion = false;
-  } else {
-    mostrarMensajeError("termsAndConditions", "");
-  }
-  return condicion;
-}
+	const terminos = document.getElementById('terminos');
+	if(campos.usuario && campos.nombre && campos.password && campos.correo && campos.telefono && terminos.checked ){
+		formulario.reset();
 
-function mostrarMensajeError(claseInput, mensaje) {
-  let elemento = document.querySelector(`.${claseInput}`);
-  elemento.lastElementChild.innerHTML = mensaje;
-}
+		document.getElementById('formulario__mensaje-exito').classList.add('formulario__mensaje-exito-activo');
+		setTimeout(() => {
+			document.getElementById('formulario__mensaje-exito').classList.remove('formulario__mensaje-exito-activo');
+		}, 5000);
 
-function enviarFormulario() {
-  form.reset();
-  form.lastElementChild.innerHTML = "Listo !!";
-}
+		document.querySelectorAll('.formulario__grupo-correcto').forEach((icono) => {
+			icono.classList.remove('formulario__grupo-correcto');
+		});
+	} else {
+		document.getElementById('formulario__mensaje').classList.add('formulario__mensaje-activo');
+	}
+});
