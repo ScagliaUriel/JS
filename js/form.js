9be484dd983fa4aa41b93ccf21b517562f919c98
Nicetastic <urielscagliarcc@gmail.com>
2022-07-26 13:54:00 -0300

    sweetAlert

--- js/form.js
+++ js/form.js
@@ -1,7 +1,6 @@
-const formulario = document.getElementById('formulario');
-const inputs = document.querySelectorAll('#formulario input');
+const formulario = document.getElementById("formulario");
+const inputs = document.querySelectorAll("#formulario input");
 var counter = 0
-// const btn = document.querySelector("#formulario__btn")
 
 if (!(localStorage.getItem("counter")!=null)){
 	localStorage.setItem("counter", 0);
@@ -26,70 +25,70 @@ const campos = {
 const validarFormulario = (e) => {
 	switch (e.target.name) {
 		case "usuario":
-			validarCampo(expresiones.usuario, e.target, 'usuario');
+			validarCampo(expresiones.usuario, e.target, "usuario");
 		break;
 		case "nombre":
-			validarCampo(expresiones.nombre, e.target, 'nombre');
+			validarCampo(expresiones.nombre, e.target, "nombre");
 		break;
 		case "password":
-			validarCampo(expresiones.password, e.target, 'password');
+			validarCampo(expresiones.password, e.target, "password");
 			validarPassword2();
 		break;
 		case "password2":
 			validarPassword2();
 		break;
 		case "correo":
-			validarCampo(expresiones.correo, e.target, 'correo');
+			validarCampo(expresiones.correo, e.target, "correo");
 		break;
 		case "telefono":
-			validarCampo(expresiones.telefono, e.target, 'telefono');
+			validarCampo(expresiones.telefono, e.target, "telefono");
 		break;
 	}
 }
 
 const validarCampo = (expresion, input, campo) => {
 	if(expresion.test(input.value)){
-		document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-incorrecto');
-		document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-correcto');
-		document.querySelector(`#grupo__${campo} i`).classList.add('fa-check-circle');
-		document.querySelector(`#grupo__${campo} i`).classList.remove('fa-times-circle');
-		document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.remove('formulario__input-error-activo');
+		document.getElementById(`grupo__${campo}`).classList.remove("formulario__grupo-incorrecto");
+		document.getElementById(`grupo__${campo}`).classList.add("formulario__grupo-correcto");
+		document.querySelector(`#grupo__${campo} i`).classList.add("fa-check-circle");
+		document.querySelector(`#grupo__${campo} i`).classList.remove("fa-times-circle");
+		document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.remove("formulario__input-error-activo");
 		campos[campo] = true;
 	} else {
-		document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-incorrecto');
-		document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-correcto');
-		document.querySelector(`#grupo__${campo} i`).classList.add('fa-times-circle');
-		document.querySelector(`#grupo__${campo} i`).classList.remove('fa-check-circle');
-		document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.add('formulario__input-error-activo');
+		document.getElementById(`grupo__${campo}`).classList.add("formulario__grupo-incorrecto");
+		document.getElementById(`grupo__${campo}`).classList.remove("formulario__grupo-correcto");
+		document.querySelector(`#grupo__${campo} i`).classList.add("fa-times-circle");
+		document.querySelector(`#grupo__${campo} i`).classList.remove("fa-check-circle");
+		document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.add("formulario__input-error-activo");
 		campos[campo] = false;
 	}
 }
 
 const validarPassword2 = () => {
-	const inputPassword1 = document.getElementById('password');
-	const inputPassword2 = document.getElementById('password2');
+	const inputPassword1 = document.getElementById("password");
+	const inputPassword2 = document.getElementById("password2");
 
 	if(inputPassword1.value !== inputPassword2.value){
-		document.getElementById(`grupo__password2`).classList.add('formulario__grupo-incorrecto');
-		document.getElementById(`grupo__password2`).classList.remove('formulario__grupo-correcto');
-		document.querySelector(`#grupo__password2 i`).classList.add('fa-times-circle');
-		document.querySelector(`#grupo__password2 i`).classList.remove('fa-check-circle');
-		document.querySelector(`#grupo__password2 .formulario__input-error`).classList.add('formulario__input-error-activo');
-		campos['password'] = false;
+		document.getElementById(`grupo__password2`).classList.add("formulario__grupo-incorrecto");
+		document.getElementById(`grupo__password2`).classList.remove("formulario__grupo-correcto");
+		document.querySelector(`#grupo__password2 i`).classList.add("fa-times-circle");
+		document.querySelector(`#grupo__password2 i`).classList.remove("fa-check-circle");
+		document.querySelector(`#grupo__password2 .formulario__input-error`).classList.add("formulario__input-error-activo");
+		campos["password"] = false;
 	} else {
-		document.getElementById(`grupo__password2`).classList.remove('formulario__grupo-incorrecto');
-		document.getElementById(`grupo__password2`).classList.add('formulario__grupo-correcto');
-		document.querySelector(`#grupo__password2 i`).classList.remove('fa-times-circle');
-		document.querySelector(`#grupo__password2 i`).classList.add('fa-check-circle');
-		document.querySelector(`#grupo__password2 .formulario__input-error`).classList.remove('formulario__input-error-activo');
-		campos['password'] = true;
+		document.getElementById(`grupo__password2`).classList.remove("formulario__grupo-incorrecto");
+		document.getElementById(`grupo__password2`).classList.add("formulario__grupo-correcto");
+		document.querySelector(`#grupo__password2 i`).classList.remove("fa-times-circle");
+		document.querySelector(`#grupo__password2 i`).classList.add("fa-check-circle");
+		document.querySelector(`#grupo__password2 .formulario__input-error`).classList.remove("formulario__input-error-activo");
+		campos["password"] = true;
 	}
 }
 
 inputs.forEach((input) => {
-	input.addEventListener('keyup', validarFormulario);
-	input.addEventListener('blur', validarFormulario);
-});
+	input.addEventListener("keyup", validarFormulario);
+	input.addEventListener("blur", validarFormulario);
+})
 
 function save_data(){
 	contacto = {}
@@ -104,42 +103,35 @@ function save_data(){
 	localStorage.setItem("counter", counter+1);
 }
 
-formulario.addEventListener('submit', (e) => {
+formulario.addEventListener("submit", (e) => {
 	e.preventDefault();
 
-	const terminos = document.getElementById('terminos');
+	const terminos = document.getElementById("terminos");
 	if(campos.usuario && campos.nombre && campos.password && campos.correo && campos.telefono && terminos.checked ){
-		save_data() 
-		formulario.reset();
+		save_data();
+		formulario.reset()
 
-		document.getElementById('formulario__mensaje-exito').classList.add('formulario__mensaje-exito-activo');
-		setTimeout(() => {
-			document.getElementById('formulario__mensaje-exito').classList.remove('formulario__mensaje-exito-activo');
-		}, 5000);
+		const Toast = Swal.mixin({
+			toast: true,
+			position: "center-end",
+			showConfirmButton: false,
+			timer: 3000,
+			timerProgressBar: true,
+			didOpen: (toast) => {
+			  toast.addEventListener("mouseenter", Swal.stopTimer)
+			  toast.addEventListener("mouseleave", Swal.resumeTimer)
+			}
+		  })
+		  
+		  Toast.fire({
+			icon: "success",
+			title: "Signed in successfully"
+		  })
 
-		document.querySelectorAll('.formulario__grupo-correcto').forEach((icono) => {
-			icono.classList.remove('formulario__grupo-correcto');
-		});
+		document.querySelectorAll(".formulario__grupo-correcto").forEach((icono) => {
+			icono.classList.remove("formulario__grupo-correcto");
+		})
 	} else {
-		document.getElementById('formulario__mensaje').classList.add('formulario__mensaje-activo');
+		document.getElementById("formulario__mensaje").classList.add("formulario__mensaje-activo");
 	}
-});
-
-//  btn.addEventListener("click", () => {
-const Toast = Swal.mixin({
-  toast: true,
-  position: 'top-end',
-  showConfirmButton: false,
-  timer: 3000,
-  timerProgressBar: true,
-  didOpen: (toast) => {
-    toast.addEventListener('mouseenter', Swal.stopTimer)
-    toast.addEventListener('mouseleave', Swal.resumeTimer)
-  }
 })
-
-Toast.fire({
-  icon: 'success',
-  title: 'Signed in successfully'
-})
-//  });
